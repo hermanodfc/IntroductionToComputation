724afc2a7582c05cf98d95a9cf14af5f40adf541
Hermano Costa <hermanodfc@gmail.com>
2019-07-22 22:49:54 -0300

    Chapter 02 - Finger 01

--- Chapter02/Finger01.py
+++ Chapter02/Finger01.py
@@ -7,6 +7,7 @@ max_odd_number = -1
 if x % 2 == 1 and x > max_odd_number:
     max_odd_number = x
     letter_name = "x"
+
 if y % 2 == 1 and y > max_odd_number:
     max_odd_number = y
     letter_name = "y"
